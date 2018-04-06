cb9d6d8b7b28de0bf876b0cec045f32d88c96fc7
SebastianGi <erwinkse@outlook.com>
2018-04-06 14:55:48 +0200

    Added -osu command

---
+++ modules/osu.py
@@ -0,0 +1,78 @@
+import discord
+import random
+import requests
+import json
+import urllib.parse
+from tinydb import TinyDB, Query
+from discord.ext import commands
+from desubot import userconfig
+from desubot import userdb
+
+osutable = userdb.table('osu')
+
+class osu():
+    def __init__(self, bot):
+        self.bot = bot
+
+    # first argument after -osu (-osu xyz) decides which subcommand will be run
+    # no argument (-osu): display this help
+    # -osu set [osu username]: ties an osu! username to your discord user
+    # -osu user [osu username(optional)]: displays data for a specific osu user
+    # -osu maps [osu beatmap/beatmapset url/id]: displays data for a specific beatmap
+    # -osu alert: enables alerts for the username tied to your discord user
+
+    @commands.command(pass_context=True)
+    async def osu(self, ctx, switchboye: str = None, argboyone: str = None, argboyetwo: str = None):
+        userboy = ctx.message.author
+        if switchboye == None:
+            await self.bot.say('soon™')
+        # SET
+        elif switchboye == "set":
+            if argboyone is None:
+                await self.bot.say('You forgot to add your osu! User')
+            else:
+                userquery = Query()
+                if osutable.count(userquery.discord_uid == userboy.id) == 0:
+                    try:
+                        temp = osuapirequest('get_user?k=' + userconfig['keys']['osuapi'] + '&u=' + argboyone)
+                        await self.bot.say(temp)
+                        osutable.insert({'discord_uid': userboy.id, 'osu_uid': argboyone})
+                        await self.bot.say('osu! user **' + temp['username'] + '** has been tied to your Discord user')
+                    except IndexError:
+                        await self.bot.say(argboyone + " doesn't seem to be a valid osu! user")
+                else:
+                    temp = osutable.get(userquery.discord_uid == userboy.id)
+                    await self.bot.say("osu! user **" + temp.get('osu_uid') + '** is currently tied to your account, use "-osu unset" to remove')
+        # UNSET
+        elif switchboye == "unset":
+            userquery = Query()
+            if osutable.count(userquery.discord_uid == userboy.id) == 1:
+                osutable.remove(userquery.discord_uid == userboy.id)
+                await self.bot.say('osu! user tied to your discord user has been removed')
+            else:
+                await self.bot.say("No osu! user currently tied to your account")
+        # USER
+        elif switchboye == "user":
+            await self.bot.say('user: soon™')
+        # MAPS
+        elif switchboye == "maps":
+            await self.bot.say('maps: soon™')
+        # ALERT
+        elif switchboye == "alert":
+            await self.bot.say('alert: soon™')
+        # EXCEPTION
+        else:
+            await self.bot.say('Unknown argument ' + switchboye)
+
+    @commands.command(description='ToDo.')
+    async def temp(self):
+        await self.bot.say('ToDo')
+
+def osuapirequest(requeststring):
+    temp = requests.get('https://osu.ppy.sh/api/' + requeststring)
+    temp = temp.json()
+    temp = temp[0]
+    return temp
+
+def setup(bot):
+    bot.add_cog(osu(bot))
